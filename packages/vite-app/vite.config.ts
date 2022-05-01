1bacff1c7073a7f5a41d454af63879fad8f744f9
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-05-01 17:54:45 +0200

    additional example

--- packages/vite-app/vite.config.ts
+++ packages/vite-app/vite.config.ts
@@ -1,7 +1,9 @@
 import { foo } from 'esm-only'
+import { foo as foo2 } from 'esm-only2'
 import { defineConfig } from 'vite'
 
 console.log('foo:', foo)
+console.log('foo2:', foo2)
 
 export default defineConfig({
   // Vite config here
